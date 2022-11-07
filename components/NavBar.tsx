44b6267f36db9d39d5d882002fe4aee271506c2f
Paman Beruang <tony762843@yahoo.co.id>
2022-11-07 23:17:13 +0700

    update public files

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -2,12 +2,18 @@ import { HStack, Spacer } from "@chakra-ui/react"
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui"
 import { FC } from "react"
 import styles from "../styles/Home.module.css"
+import dynamic from 'next/dynamic';
 
 const NavBar: FC = () => {
+  const WalletMultiButtonDynamic = dynamic(
+    async () => (await import('@solana/wallet-adapter-react-ui')).WalletMultiButton,
+    { ssr: false }
+);
+
   return (
     <HStack width="full" padding={4}>
       <Spacer />
-      <WalletMultiButton className={styles["wallet-adapter-button-trigger"]} />
+      <WalletMultiButtonDynamic className={styles["wallet-adapter-button-trigger"]} />
     </HStack>
   )
 }
