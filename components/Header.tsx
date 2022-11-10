8afa2eb84e8b22cb40fe61dfb9079c7084409f4f
Paman Beruang <tony762843@yahoo.co.id>
2022-11-11 00:09:01 +0700

    button fix

--- components/Header.tsx
+++ components/Header.tsx
@@ -13,7 +13,7 @@ const Header: FC = () => {
   return (
     <header className="sticky top-0 z-50 flex flex-row bg-white shadow-md p-5 md:px-10">
       {/* left */}
-      <div className="basis-1/4 relative flex items-center h-10 cursor-pointer my-auto">
+      <div className="md:basis-1/4 relative flex items-center h-10 cursor-pointer my-auto">
         <Image 
           src={islandLogo}
           alt="islandLogo"
@@ -24,17 +24,19 @@ const Header: FC = () => {
       </div> 
 
       {/* middle */}
-      <div className="basis-1/2 flex items-center space-x-2">
-        <div className=" bg-gray-700 p-2 text-white w-8 h-8 rounded-full flex justify-center items-center">1</div>
-        <p>Connect your wallet</p>
-        <div className=" bg-gray-700 p-2 text-white w-8 h-8 rounded-full flex justify-center items-center">2</div>
-        <p>Add your information</p>
-        <div className=" bg-gray-700 p-2 text-white w-8 h-8 rounded-full flex justify-center items-center">3</div>
-        <p>Appear on Anybodies' Island!</p>
+      <div className="md:basis-1/2 hidden md:inline">
+        <div className="flex items-center space-x-2">
+          <div className=" bg-gray-700 p-2 text-white w-8 h-8 rounded-full flex justify-center items-center">1</div>
+          <p>Connect your wallet</p>
+          <div className=" bg-gray-700 p-2 text-white w-8 h-8 rounded-full flex justify-center items-center">2</div>
+          <p>Add your information</p>
+          <div className=" bg-gray-700 p-2 text-white w-8 h-8 rounded-full flex justify-center items-center">3</div>
+          <p>Appear on Anybodies' Island!</p>
+        </div>
       </div>
 
       {/* right */}
-      <div className="basis-1/4 flex items-center space-x-4 justify-end">
+      <div className="md:basis-1/4 flex items-center space-x-4 justify-end">
         <WalletMultiButtonDynamic />
       </div>
     </header>
