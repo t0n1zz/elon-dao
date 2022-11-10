64e055ab8770bbe1cd4252462b6239fa3bc3180e
Paman Beruang <tony762843@yahoo.co.id>
2022-11-11 06:08:55 +0700

    styling update

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,13 +9,30 @@ const Footer: FC = () => {
     <footer className="text-center p-5">
      <p>Visually see Anybodies holder around you</p>
      <p>Ease meet-ups with fellow Anybodies</p>
-     <div className="flex items-center justify-center space-x-4 pt-5">
+
+     {/* desktop */}
+     <div className="hidden md:inline">
+      <div className="flex items-center justify-center space-x-4 pt-5">
+          <p>Brought to you by</p>
+          <Image 
+            src={elonDAOLogo}
+            alt="elonDaoLogo"
+          />
+          <p>Developed by PamanBeruang</p>
+      </div>
+     </div>
+
+     {/* mobile */}
+     <div className="md:hidden">
+      <div className="text-center pt-5">
         <p>Brought to you by</p>
         <Image 
           src={elonDAOLogo}
           alt="elonDaoLogo"
+          className="mx-auto pt-2 pb-2"
         />
         <p>Developed by PamanBeruang</p>
+      </div>
      </div>
     </footer>
   )
