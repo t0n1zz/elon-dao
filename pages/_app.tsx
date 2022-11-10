fb5c9ddcc9fecce48583b087b1418254cee9fb4b
Paman Beruang <tony762843@yahoo.co.id>
2022-11-11 06:02:23 +0700

    styling update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,3 @@
-require("@solana/wallet-adapter-react-ui/styles.css")
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import WalletContextProvider from '../components/WalletContextProvider'
